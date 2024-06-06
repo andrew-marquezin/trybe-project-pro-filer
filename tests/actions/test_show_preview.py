da886532e13d3f7bf3307dd6c5840aac10646708
andrew-marquezin <andrewmmarinho@gmail.com>
2024-06-06 17:22:17 -0300

    caso de teste adicionado

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -28,6 +28,39 @@ First 5 directories: ['src', 'src/utils']
             """Found 0 files and 0 directories
 """,
         ),
+        (
+            {
+                "all_files": [
+                    "src/__init__.py",
+                    "src/app.py",
+                    "src/utils/__init__.py",
+                    "src/__main__.py",
+                    "src/tests/test_app.py",
+                    "src/tests/__init__.py",
+                ],
+                "all_dirs": [
+                    "src",
+                    "src/utils",
+                    "src/tests",
+                    "src/model",
+                    "src/service",
+                    "src/controller",
+                ],
+            },
+            (
+                "Found 6 files and 6 directories\n"
+                "First 5 files: ['src/__init__.py', "
+                "'src/app.py', "
+                "'src/utils/__init__.py', "
+                "'src/__main__.py', "
+                "'src/tests/test_app.py']\n"
+                "First 5 directories: ['src', "
+                "'src/utils', "
+                "'src/tests', "
+                "'src/model', "
+                "'src/service']\n"
+            ),
+        ),
     ],
 )
 def test_show_preview(context, output, capsys):
