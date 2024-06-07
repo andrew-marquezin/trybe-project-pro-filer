ad69234b5715486d06e7eae714780dfcea89c336
andrew-marquezin <andrewmmarinho@gmail.com>
2024-06-07 17:06:36 -0300

    mais casos de teste

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -31,3 +31,43 @@ def test_inexistent_file(capsys):
     captured = capsys.readouterr()
 
     assert captured.out == "File '????' does not exist\n"
+
+
+def test_no_extension_file(tmp_path, capsys):
+    file_name = "banana"
+    file_path = tmp_path / file_name
+    file_path.touch()
+    context = {"base_path": str(file_path)}
+    _, file_extension = os.path.splitext(file_name)
+    mod_date = datetime.date.fromtimestamp(os.path.getmtime(file_path))
+
+    show_details(context)
+    captured = capsys.readouterr()
+
+    assert captured.out == (
+        f"File name: {file_name}\n"
+        f"File size in bytes: {os.path.getsize(file_path)}\n"
+        f"File type: {'directory' if os.path.isdir(file_path) else 'file'}\n"
+        f"File extension: {file_extension or '[no extension]'}\n"
+        f"Last modified date: {mod_date}\n"
+    )
+
+
+def test_directory(tmp_path, capsys):
+    file_name = "cacho"
+    file_path = tmp_path / file_name
+    file_path.mkdir()
+    context = {"base_path": str(file_path)}
+    _, file_extension = os.path.splitext(file_name)
+    mod_date = datetime.date.fromtimestamp(os.path.getmtime(file_path))
+
+    show_details(context)
+    captured = capsys.readouterr()
+
+    assert captured.out == (
+        f"File name: {file_name}\n"
+        f"File size in bytes: {os.path.getsize(file_path)}\n"
+        f"File type: {'directory' if os.path.isdir(file_path) else 'file'}\n"
+        f"File extension: {file_extension or '[no extension]'}\n"
+        f"Last modified date: {mod_date}\n"
+    )
